16bf2ac10c5276b9ceca4edefba9a340beba69cb
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2023-03-29 16:06:46 +0530

    Designed the dynamic grid with hover effect

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
-let rows = 16;
-let columns = 16;
-// container block
 const body = document.querySelector('body');
-
+const btn = document.createElement('button');
+btn.classList.add('btn');
+btn.textContent = 'Click to change size of grid';
+body.appendChild(btn);
 const container = document.createElement('div');
 container.classList.add('container');
 body.appendChild(container);
-
 const containerBlock = document.querySelector('.container');
 
-for(let i = 0; i < rows; i++){
-    rowElement = document.createElement('div');
-    rowElement.classList.add('row');
-    for (let j = 0; j < columns; j++) {
-        colElement = document.createElement('div');
-        colElement.classList.add('col');
-        rowElement.appendChild(colElement);
+function changeGridSize(newSize) {  //function to the change the size of grid
+    let newRow = newSize;
+    let newColumn = newSize;
+    container.innerHTML = '';
+    for(let i = 0; i < newRow; i++){
+        rowElement = document.createElement('div');
+        rowElement.classList.add('row');
+        for (let j = 0; j < newColumn; j++) {
+            colElement = document.createElement('div');
+            colElement.classList.add('col');
+            rowElement.appendChild(colElement);
+        }
+        containerBlock.appendChild(rowElement);
     }
-    containerBlock.appendChild(rowElement);
 }
+
+function hoverEffect() {       //function that enables hover effect
+    const items = document.querySelectorAll('.col');
+    items.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        item.style.backgroundColor = 'blue';
+    });
+    item.addEventListener('mouseout', () => {
+        item.style.backgroundColor = 'white';
+    });
+});
+}
+
+const btnGrid = document.querySelector('.btn');
+btnGrid.addEventListener('click', () => {
+    size = prompt("Enter the size of grid between 0 and 100:");
+    changeGridSize(size);
+    hoverEffect();
+});
+changeGridSize(16);
+hoverEffect();
